ab3b54f9a49e25c980ff5d562757d258cb5ceeca
chzh859@naver.com <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>
2024-09-17 18:20:21 +0900

    # 스프링 배치 1일차

--- src/main/java/com/example/batch/jobs/MyJobConfig.java
+++ src/main/java/com/example/batch/jobs/MyJobConfig.java
@@ -6,28 +6,29 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class MyJobConfig {
 
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-
     @Bean
-    public Job myJob() {
-        return jobBuilderFactory.get("myJob")
-                .start(myStep())
+    public Job myJob(JobRepository jobRepository) {
+        return new JobBuilder("myJob", jobRepository)
+                .start(myStep(jobRepository))
                 .build();
     }
 
     @Bean
-    public Step myStep() {
-        return stepBuilderFactory.get("myStep")
+    public Step myStep(JobRepository jobRepository) {
+        return new StepBuilder("myStep", jobRepository)
                 .tasklet(((contribution, chunkContext) -> {
                         log.info("step started");
                         return RepeatStatus.FINISHED;
