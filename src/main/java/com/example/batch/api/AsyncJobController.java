8ffb231065540db2a0034e545b43dc6c5f020a38
beomshin.kim <chzh859@naver.com>
2024-10-22 19:53:19 +0900

    # flow parallel step 적용기

--- src/main/java/com/example/batch/api/AsyncJobController.java
+++ src/main/java/com/example/batch/api/AsyncJobController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class AsyncJobController {
     private final JobLauncher jobLauncher;
     private final Job asyncJob;
     private final Job multiThreadStepJob;
+    private final Job parallelStepJob;
 
     @GetMapping("/async/job/run")
     public void runJob() throws Exception {
@@ -32,6 +34,14 @@ public class AsyncJobController {
     public void runJo2() throws Exception {
         log.info("병렬수항테스트(MultiThreadStepTestJob) 시작");
         jobLauncher.run(multiThreadStepJob, new JobParametersBuilder()
+                .addDate("startTime", new Date())  // 고유한 파라미터
+                .toJobParameters());
+    }
+
+    @GetMapping("/async/job3/run")
+    public void runJo3() throws Exception {
+        log.info("병렬수항테스트(parallelStepJob) 시작");
+        jobLauncher.run(parallelStepJob, new JobParametersBuilder()
                 .addDate("startTime", new Date())  // 고유한 파라미터
                 .toJobParameters());
     }
